30ee68a68b50885ae7ed7786ae79d6ef2f368cd5
asaf <asafebgi@gmail.com>
2022-12-14 08:28:39 -0800

    osc

--- main.lua
+++ main.lua
@@ -4,8 +4,10 @@ require 'nudge/nudge'
 require 'move/move'
 require 'clone/clone'
 require 'enums'
+require 'osc_client'
+require 'osc_server'
+
+-- init_osc_server()
+-- init_osc_client()
 
 
-function main()
-    
-end
---
+++ osc_client.lua
@@ -0,0 +1,44 @@
+function init_osc_client() 
+  local OscMessage = renoise.Osc.Message
+  local OscBundle = renoise.Osc.Bundle
+  print("init osc client")
+
+  -- open a socket connection to the server
+  local client, socket_error = renoise.Socket.create_client(
+    "192.168.1.48",  10000, renoise.Socket.PROTOCOL_UDP)
+    
+  if (socket_error) then 
+    renoise.app():show_warning(("Failed to start the " .. 
+      "OSC client. Error: '%s'"):format(socket_error))
+    return
+  end
+
+  -- construct and send messages
+  client:send(
+    OscMessage("/someone/transport/start")
+  )
+
+  client:send(
+    OscMessage("/someone/transport/bpm", { 
+      {tag="f", value=127.5} 
+    })
+  )
+
+  -- construct and send bundles
+  client:send(
+    OscBundle(os.clock(), OscMessage("/someone/transport/start"))
+  )
+
+  local message1 = OscMessage("/some/message")
+
+  local message2 = OscMessage("/another/one", { 
+    {tag="b", value="with some blob data"},
+    {tag="s", value="and a string"} 
+  })
+
+  client:send(
+    OscBundle(os.clock(), {message1, message2})
+  )
+
+end
+
