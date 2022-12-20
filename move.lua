0dde984825a026b446ca770bdb46e99d8d509018
asaf <asafebgi@gmail.com>
2022-12-19 20:17:48 -0800

    prettier

--- move.lua
+++ move.lua
@@ -1,73 +1,62 @@
 -- MOVE UP
-renoise.tool():add_keybinding {
+renoise.tool():add_keybinding{
   name = "Global:Tools:cc.asaf Move Up",
-  invoke = function()
-    moveUp()
-  end
+  invoke = function() moveUp() end
 }
 
-renoise.tool():add_menu_entry {
+renoise.tool():add_menu_entry{
   name = "Main Menu:Tools:cc.asaf:Move Up",
-  invoke = function()
-    moveUp()
-  end  
+  invoke = function() moveUp() end
 }
 
 -- MOVE DOWN
-renoise.tool():add_keybinding {
+renoise.tool():add_keybinding{
   name = "Global:Tools:cc.asaf Move Down",
   invoke = function()
+
+    local s = renoise.song().selection_in_pattern
+    if s.start_line ~= s.end_line then return selectionMoveDown() end
+
+    for k, v in pairs(s) do print(k, v) end
+
     moveDown()
   end
 }
 
-renoise.tool():add_menu_entry {
+renoise.tool():add_menu_entry{
   name = "Main Menu:Tools:cc.asaf:Move Down",
-  invoke = function()
-    moveDown()
-  end  
+  invoke = function() moveDown() end
 }
 
 -- MOVE LEFT
-renoise.tool():add_keybinding {
+renoise.tool():add_keybinding{
   name = "Global:Tools:cc.asaf Move Left",
-  invoke = function()
-    moveLeft()
-  end
+  invoke = function() moveLeft() end
 }
 
-renoise.tool():add_menu_entry {
+renoise.tool():add_menu_entry{
   name = "Main Menu:Tools:cc.asaf:Move Left",
-  invoke = function()
-    moveLeft()
-  end  
+  invoke = function() moveLeft() end
 }
 
 -- MOVE RIGHT
-renoise.tool():add_keybinding {
+renoise.tool():add_keybinding{
   name = "Global:Tools:cc.asaf Move Right",
-  invoke = function()
-    moveRight()
-  end
+  invoke = function() moveRight() end
 }
 
-renoise.tool():add_menu_entry {
+renoise.tool():add_menu_entry{
   name = "Main Menu:Tools:cc.asaf:Move Right",
-  invoke = function()
-    moveRight()
-  end  
+  invoke = function() moveRight() end
 }
 
 function moveUp()
   local song = renoise.song()
-  if not song.selected_note_column then
-    return
-  end
-  local subcol, note, note_above = get_current_subcol(), get_current_note(), get_above_note()
+  if not song.selected_note_column then return end
+  local subcol, note, note_above = get_current_subcol(), get_current_note(),
+                                   get_above_note()
 
-  if note.note_value == 121 then
-    return
-  end
+  if note.note_value == 121 then return end
 
   note_above.note_value = note.note_value
   note_above.instrument_value = note.instrument_value
@@ -88,17 +77,13 @@ end
 
 function moveRight()
   local song = renoise.song()
-  if not song.selected_note_column then
-    return
-  end
-  
+  if not song.selected_note_column then return end
+
   local subcol = get_current_subcol()
   local note = get_current_note()
   local note_right = get_right_note()
 
-  if note.note_value == 121 then
-    return
-  end
+  if note.note_value == 121 then return end
 
   note_right.note_value = note.note_value
   note.note_value = 121
@@ -117,16 +102,12 @@ end
 
 function moveLeft()
   local song = renoise.song()
-  if not song.selected_note_column then
-    return
-  end
+  if not song.selected_note_column then return end
   local subcol = get_current_subcol()
   local note = get_current_note()
   local note_left = get_left_note()
 
-  if note.note_value == 121 then
-    return
-  end
+  if note.note_value == 121 then return end
 
   note_left.note_value = note.note_value
   note.note_value = 121
@@ -142,20 +123,14 @@ function moveLeft()
   note.delay_value = 0
 end
 
-
-
 function moveDown()
   local song = renoise.song()
-  if not song.selected_note_column then
-    return
-  end
+  if not song.selected_note_column then return end
   local subcol = get_current_subcol()
   local note = get_current_note()
   local note_below = get_below_note()
 
-  if note.note_value == 121 then
-    return
-  end
+  if note.note_value == 121 then return end
 
   note_below.note_value = note.note_value
   note_below.instrument_value = note.instrument_value
@@ -172,4 +147,19 @@ function moveDown()
   note.note_value = 121
   note.effect_number_value = 0
   note.effect_amount_value = 0
+end
+
+function selectionMoveDown()
+  local song = renoise.song()
+  print('selection move')
+
+  local sp = song.selection_in_pattern
+  song.selection_in_pattern = {
+    start_line = sp.start_line + 1,
+    end_line = sp.end_line + 1
+  }
+  -- sp = {}
+  -- sp.start_line = sp.start_line + 1
+  -- sp.end_line = sp.end_line + 1
+
 end
