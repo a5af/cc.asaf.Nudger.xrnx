e70c6b208d5e2f5ea1683052fb995b2861551659
asaf <asafebgi@gmail.com>
2022-12-29 00:29:08 -0800

    move clear

--- main.lua
+++ main.lua
@@ -42,7 +42,7 @@ function get_current_note()
   local cur = get_cur_line_track_col_pattern_inst_phrase()
   return
     song.patterns[cur.pattern].tracks[cur.track].lines[cur.line]:note_column(
-      cur.col)
+      cur.note_col)
 end
 
 function get_current_selected()
@@ -63,15 +63,15 @@ end
 function get_left_note()
   local song = renoise.song()
   local cur = get_cur_line_track_col_pattern_inst_phrase()
-  if cur.col == 0 then
+  if cur.note_col == 0 then
     if cur.track == 0 then return end
     cur.track = cur.track - 1
   else
-    cur.col = cur.col - 1
+    cur.note_col = cur.note_col - 1
   end
   return
     song.patterns[cur.pattern].tracks[cur.track].lines[cur.line]:note_column(
-      cur.col)
+      cur.note_col)
 end
 
 function get_right_note()
@@ -79,15 +79,15 @@ function get_right_note()
   if not song.selected_note_column then return end
   local cur = get_cur_line_track_col_pattern_inst_phrase()
 
-  if cur.col == get_col_count() - 1 then
+  if cur.note_col == get_col_count() - 1 then
     if cur.track == get_track_count() - 1 then return end
     cur.track = cur.track - 1
   else
-    cur.col = cur.col - 1
+    cur.note_col = cur.note_col - 1
   end
   return
     song.patterns[cur.pattern].tracks[cur.track].lines[cur.line]:note_column(
-      cur.col)
+      cur.note_col)
 
 end
 
@@ -99,7 +99,7 @@ function get_above_note()
   if above_line < 0 then return end
   return
     song.patterns[cur.pattern].tracks[cur.track].lines[above_line]:note_column(
-      cur.col)
+      cur.note_col)
 end
 
 function get_line_count()
@@ -134,7 +134,7 @@ function get_below_note()
   if below_line >= get_line_count() then return end
   return
     song.patterns[cur.pattern].tracks[cur.track].lines[below_line]:note_column(
-      cur.col)
+      cur.note_col)
 end
 
 function get_phrase()
@@ -380,8 +380,167 @@ function nudgeDown()
 
 end
 
-require 'clear'
-require 'move'
+-- CLEAR
+renoise.tool():add_keybinding{
+  name = "Global:Tools:cc.asaf Clear",
+  invoke = function() clear() end
+}
+
+renoise.tool():add_menu_entry{
+  name = "Main Menu:Tools:cc.asaf:Clear",
+  invoke = function() clear() end
+}
+
+function clear()
+  local song = renoise.song()
+  if not song.selected_note_column then return end
+  local subcol = get_current_subcol()
+  local note = get_current_note()
+  clear_row(note)
+end
+
+-- MOVE UP
+renoise.tool():add_keybinding{
+  name = "Global:Tools:cc.asaf Move Up",
+  invoke = function()
+    local s = renoise.song().selection_in_pattern
+    if s.start_line ~= s.end_line then return selectionMoveUp() end
+    moveUp()
+  end
+}
+
+renoise.tool():add_menu_entry{
+  name = "Main Menu:Tools:cc.asaf:Move Up",
+  invoke = function() moveUp() end
+}
+
+-- MOVE DOWN
+renoise.tool():add_keybinding{
+  name = "Global:Tools:cc.asaf Move Down",
+  invoke = function()
+    local s = renoise.song().selection_in_pattern
+    if s.start_line ~= s.end_line then return selectionMoveDown() end
+    moveDown()
+  end
+}
+
+renoise.tool():add_menu_entry{
+  name = "Main Menu:Tools:cc.asaf:Move Down",
+  invoke = function() moveDown() end
+}
+
+-- MOVE LEFT
+renoise.tool():add_keybinding{
+  name = "Global:Tools:cc.asaf Move Left",
+  invoke = function() moveLeft() end
+}
+
+renoise.tool():add_menu_entry{
+  name = "Main Menu:Tools:cc.asaf:Move Left",
+  invoke = function() moveLeft() end
+}
+
+-- MOVE RIGHT
+renoise.tool():add_keybinding{
+  name = "Global:Tools:cc.asaf Move Right",
+  invoke = function() moveRight() end
+}
+
+renoise.tool():add_menu_entry{
+  name = "Main Menu:Tools:cc.asaf:Move Right",
+  invoke = function() moveRight() end
+}
+
+function moveUp()
+  local song = renoise.song()
+  if not song.selected_note_column then return end
+  local subcol, note, note_above = get_current_subcol(), get_current_note(),
+                                   get_above_note()
+  if note.note_value == 121 then return end
+  copy_note_values(note, note_above)
+  clear_row(note)
+  song.selected_line_index = song.selected_line_index - 1
+end
+
+function moveDown()
+  local song = renoise.song()
+  if not song.selected_note_column then return end
+  local subcol = get_current_subcol()
+  local note = get_current_note()
+  local note_below = get_below_note()
+
+  if note.note_value == 121 then return end
+
+  copy_note_values(note, note_below)
+  clear_row(note)
+  song.selected_line_index = song.selected_line_index + 1
+
+end
+
+function selectionMoveDown()
+  local song = renoise.song()
+  song.selection_in_pattern = move_selection(1)
+end
+
+function move_selection(incr)
+  local song = renoise.song()
+  local sp = song.selection_in_pattern
+  return {
+    start_line = sp.start_line + incr,
+    end_line = sp.end_line + incr,
+    start_track = sp.start_track,
+    end_track = sp.end_track
+  }
+end
+
+function selectionMoveUp()
+  local song = renoise.song()
+  song.selection_in_pattern = move_selection(-1)
+
+  local sp = song.selection_in_pattern
+  local cur = get_cur_line_track_col_pattern_inst_phrase()
+
+  local first_line =
+    song.patterns[cur.pattern].tracks[cur.track].lines[sp.start_line]
+
+  for l in range(sp.start_line + 1, sp.end_line) do
+    song.patterns[cur.pattern].tracks[cur.track].lines[sp.start_line] =
+      song.patterns[cur.pattern].tracks[cur.track].lines[l]
+  end
+
+  song.patterns[cur.pattern].tracks[cur.track].lines[sp.end_line] =
+    song.patterns[cur.pattern].tracks[cur.track].lines[sp.end_line - 1]
+
+end
+
+function moveRight()
+
+  local song = renoise.song()
+  if not song.selected_note_column then return end
+
+  local subcol = get_current_subcol()
+  local note = get_current_note()
+  local note_right = get_right_note()
+
+  if note.note_value == 121 then return end
+  copy_note_values(note, note_right)
+  clear_row(note)
+
+end
+
+function moveLeft()
+  local song = renoise.song()
+  if not song.selected_note_column then return end
+  local subcol = get_current_subcol()
+  local note = get_current_note()
+  local note_left = get_left_note()
+
+  if note.note_value == 121 then return end
+
+  copy_note_values(note, note_left)
+  clear_row(note)
+end
+
 require 'clone'
 require 'constants'
 
